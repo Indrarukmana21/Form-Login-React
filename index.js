8b66f0b7f2d922e60bab15b763b8d5375cb34f9a
Indrarukmana21 <119818563+Indrarukmana21@users.noreply.github.com>
2023-02-20 10:32:05 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express           = require('express');
+const app               = express();
+const bodyParser        = require('body-parser');
+const PORT              = 3001
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use{bodyParser.json()};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.listen(PORT, (req,res) => {
+    console.log('server run port ${PORT}')
+})
